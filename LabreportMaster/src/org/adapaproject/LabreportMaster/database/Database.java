8dbe00a19d6e706b8351c63a2dce33e7dd7fa5d1
setarosd <setarosd@wfu.edu>
2017-01-13 08:21:10 -0500

    commented out semiramisia database and activated database on server.

--- LabreportMaster/src/org/adapaproject/LabreportMaster/database/Database.java
+++ LabreportMaster/src/org/adapaproject/LabreportMaster/database/Database.java
@@ -10,13 +10,13 @@ package org.adapaproject.LabreportMaster.database;
 public class Database {
 	
 	//TODO for biolabs server
-/*	private static final String USERNAME = "labreports";
+	private static final String USERNAME = "labreports";
 	private static final String PASSWORD = "schreibGut#1999";
-	private static final String CONN_STRING = "jdbc:mysql://localhost/labreport_info";*/
-	//for semiramisia
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "Malen2Zahlen587*";
 	private static final String CONN_STRING = "jdbc:mysql://localhost/labreport_info";
+	//for semiramisia
+/*	private static final String USERNAME = "root";
+	private static final String PASSWORD = "Malen2Zahlen587*";
+	private static final String CONN_STRING = "jdbc:mysql://localhost/labreport_info";*/
 
 
 	public static String getUsername() {
