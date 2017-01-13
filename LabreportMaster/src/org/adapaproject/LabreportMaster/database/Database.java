0eacc72043abe2c1654ffb30f09f8ce3c00b0cf6
setarosd <setarosd@wfu.edu>
2017-01-13 08:31:03 -0500

    cleanup commented out sections

--- LabreportMaster/src/org/adapaproject/LabreportMaster/database/Database.java
+++ LabreportMaster/src/org/adapaproject/LabreportMaster/database/Database.java
@@ -9,14 +9,9 @@ package org.adapaproject.LabreportMaster.database;
  */
 public class Database {
 	
-	//TODO for biolabs server
 	private static final String USERNAME = "labreports";
 	private static final String PASSWORD = "schreibGut#1999";
 	private static final String CONN_STRING = "jdbc:mysql://localhost/labreport_info";
-	//for semiramisia
-/*	private static final String USERNAME = "root";
-	private static final String PASSWORD = "Malen2Zahlen587*";
-	private static final String CONN_STRING = "jdbc:mysql://localhost/labreport_info";*/
 
 
 	public static String getUsername() {
