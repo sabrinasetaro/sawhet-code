fa4568aa29d5071ef578587335c4132c06447093
setarosd <setarosd@wfu.edu>
2017-08-29 11:12:22 -0400

    added commented code for biolabs

--- LabreportMaster/src/org/adapaproject/LabreportMaster/database/Database.java
+++ LabreportMaster/src/org/adapaproject/LabreportMaster/database/Database.java
@@ -9,9 +9,15 @@ package org.adapaproject.LabreportMaster.database;
  */
 public class Database {
 	
+	//for localhost
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "Malen2Zahlen587*";
 	private static final String CONN_STRING = "jdbc:mysql://localhost/labreports2";
+	
+	//for biolabs
+/*	private static final String USERNAME = "labreports";
+	private static final String PASSWORD = "schreibGut#1999";
+	private static final String CONN_STRING = "jdbc:mysql://localhost/labreports2"; */
 
 
 	public static String getUsername() {
