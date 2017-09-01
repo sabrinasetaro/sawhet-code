81c51ec4f5c498c0d2e4176842148d1d18bd12b0
setarosd <setarosd@wfu.edu>
2017-09-01 13:32:53 -0400

    deleted testing code

--- LabreportMaster/src/org/adapaproject/LabreportMaster/Main.java
+++ LabreportMaster/src/org/adapaproject/LabreportMaster/Main.java
@@ -44,8 +44,6 @@ public class Main {
 	 */
 	public static void main(String[] args) throws GateException, IOException {
 		
-		System.out.println("I'm in main");
-		
 		FileHandler handler = null;
 		try {
 			handler = new FileHandler("LabreportLog.xml");
